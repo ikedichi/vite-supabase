5b906556226a5dd5f25d32aadaa1829653729162
chinemerem ikedichi <m1@chinemerems-MacBook-Pro.local>
2022-12-28 00:52:16 -0500

    diplayed is_deleted value created file for supapbase integration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Forms from './Forms'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Forms></Forms>
+  )
+}
+
+export default App
