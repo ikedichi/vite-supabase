d4d6cf1a316828df1f00590533b589e27b5d1e34
Victor <c.ikedichi@gmail.com>
2023-01-09 14:44:09 -0500

    creating form using MUI

--- src/Forms.tsx
+++ src/Forms.tsx
@@ -1,127 +1,178 @@
 import { FormEventHandler, useEffect, useState } from 'react';
 import './App.css';
-import { FaTrash } from 'react-icons/fa';
+import { FaArrowAltCircleRight, FaTrash } from 'react-icons/fa';
+import { supabase } from './supabase';
+import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient';
+// import {DataBase} from 'lib/database.types'
 
 type Comment = {
-    first_name: string;
-    last_name: string;
-    comment: string;
-    schedule_date: string;
-    email: string;
-    id: number;
-    is_deleted: boolean;
+  first_name: string;
+  last_name: string;
+  comment: string;
+  schedule_date: string;
+  email: string;
+  id: number;
+  is_deleted: boolean;
 
 }
 
-function Forms(){
+function Forms() {
   // const [is_deleted, setIs_Deleted] = useState(false)
-   const  [isEmpty, setIsEmpty] = useState(false);
-    const [comments, setComments] = useState<Comment[]>();
-    const [form, setForm] = useState<any>({});
-    const [firstName, SetFirstName] = useState([]);
-    const [lastName, SetLastName] = useState([]);
-    const [email, SetEmail] = useState([]);
-    
-    const getData = async (commentId: number) => {
-      const result = await fetch(
-        'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments' ,
-        {
-          headers: {
-            apikey:
-              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-          },
-        },
-        // SetFirstName(result.first_name)
-      );
-      const data = await result.json() as Comment[] ;
-      const filteredData = data.filter( comment=>comment);
-      setComments(filteredData);
-      // console.log(data)
-    };
-    useEffect(() => {
-      getData();
-    }, []);
-  
-    async function onFormSubmit(event: React.FormEvent) {
-      event.preventDefault();
-      if(!form.first_name || !form.last_name || !form.email || !form.comment ){
-        console.log('please fill in all fields')
-        return;
-        
-      } else{console.log('w');}
-  
-      const result = await fetch(
-        'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            apikey:
-              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-          },
-          body: JSON.stringify(form),
-        }
-      );
-      console.log(result);
-      getData();
-      
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [comments, setComments] = useState<Comment[]>();
+  const [form, setForm] = useState<any>({});
+  const [firstName, SetFirstName] = useState([]);
+  const [lastName, SetLastName] = useState([]);
+  const [email, SetEmail] = useState([]);
+  const [date, setDate] = useState()
 
-    }
+  useEffect(() => {
+    getData();
+  }, []);
 
-
-    async function deleteComment(commentId: number) {
-        // Call API to delete comment
-        const result = await fetch(
-            'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments?id=eq.' + commentId,
-            {
-              method: 'DELETE',
-              headers: {
-                apikey:
-                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-              }
-            }
-        );
-
-        getData();
-    }
-
-    async function isDeleted(commentId: number) {
-        // TODO: Add code to update comment
-        let is_deleted = true
-        const result = await fetch(
-            'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments?id=eq.' + commentId,
-            {
-              method: 'put',
-              headers: {
-                'Content-Type': 'application/json',
-                apikey:
-                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-                  },
-                  body: JSON.stringify({ is_deleted: true })
-              // comments.is_deleted
-            } 
-        ) 
-        // { comments?.map(comment=>{comment.is_deleted = true})
+  async function getData() {
+    try {
+      const { data, error } = await supabase
+        .from('comments')
+        .select()
+      if (error) throw error;
+      if (data != null) {
+        setComments(data)
       }
+    } catch (error) {
+      console.log(error.message)
+    }
 
-        // getData();
-        
+  }
 
-    
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+  // const getData = async (commentId: number) => {
+  //   const result = await fetch(
+  //     'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments',
+  //     {
+  //       headers: {
+  //         apikey:
+  //           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
+  //       },
+  //     },
+  //     // SetFirstName(result.first_name)
+  //   );
+  //   const data = await result.json() as Comment[];
+  //   const filteredData = data.filter(comment => comment);
+  //   setComments(filteredData);
+  //   // console.log(data)
+  // };
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
+
+  async function onFormSubmit() {
+    try {
+      const { error } = await supabase
+        .from('comments')
+        .insert({ name:firstName,
+                  last: lastName,
+            // comment: comments,
+                  schedule_date: date,
+            email: email,})
+      if (error) throw error;
+      if (data != null) {
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+    getData();
+  }
+
+
+
+
+
+
+//   async function onFormSubmit(event: React.FormEvent) {
+//     event.preventDefault();
+//     if (!form.first_name || !form.last_name || !form.email || !form.comment) {
+//       console.log('please fill in all fields')
+//       return;
+
+//     } else { console.log('w'); }
+
+//     const result = await fetch(
+//       'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments',
+//       {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           apikey:
+//             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
+//         },
+//         body: JSON.stringify(form),
+//       }
+//     );
+//     console.log(result);
+//     getData();
+// }
+
+
+  async function deleteComment(commentId: number) {
+    // Call API to delete comment
+    const result = await fetch(
+      'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments?id=eq.' + commentId,
+      {
+        method: 'DELETE',
+        headers: {
+          apikey:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
+        }
+      }
+    );
+
+    getData();
+  }
+
+  async function isDeleted(commentId: number) {
+    // TODO: Add code to update comment
+    let is_deleted = true
+    const result = await fetch(
+      'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments?id=eq.' + commentId,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          apikey:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
+        },
+        body: JSON.stringify({ is_deleted: true })
+      },
+
+    )
+    // getData();
+    // { comments?.map(comment=>{comment.is_deleted = true})
+  }
+
+
+
+
+
 
   //   function isDeleted(commentId: number){ comments?.map(comment=>{comment.is_deleted = true})
   // }
 
-      function selectPerson(){
-       
-      }
+  function selectPerson() {
+
+  }
 
 
-    return ( 
+  return (
     <div>
 
-<table border={1}>
-      
+      <table border={1}>
+
         <tr>
           <th>ID</th>
           <th>First Names</th>
@@ -133,72 +184,72 @@ function Forms(){
           <th>Action</th>
           <th>Is Deleted</th>
         </tr>
-    
+
         {comments && comments.map(comment => {
-            return <tr key={comment.id}>
-                <td>{comment.id}</td>
-                <td>{comment.first_name}</td>
-                <td>{comment.last_name}</td>
-                <td>{comment.email}</td>
-                <td> {comment.comment}</td>
-                <td> <button>change</button> </td>
-                <td>{comment.schedule_date}</td>
-                <td><FaTrash onClick={() => {
-              
-              deleteComment(comment.id)
-                }} /></td>
-                <td>{JSON.stringify(comment.is_deleted)}</td>
-            </tr>
+          return <tr key={comment.id}>
+            <td>{comment.id}</td>
+            <td>{comment.first_name}</td>
+            <td>{comment.last_name}</td>
+            <td>{comment.email}</td>
+            <td> {comment.comment}</td>
+            <td> <button>change</button> </td>
+            <td>{comment.schedule_date}</td>
+            <td><FaTrash onClick={() => {
+
+            deleteComment(comment.id)
+            }} /></td>
+            <td>{JSON.stringify(comment.is_deleted)}</td>
+          </tr>
         })}
-        
+
       </table>
 
-     
+
 
       <form onSubmit={onFormSubmit} id="form">
         <div>
           <input id="first_name" placeholder="first name"
-  
-           onChange={(e) => {
-            setForm((prev) => ({ ...prev, first_name: e.target.value }));
-          }} ></input>
+
+            onChange={(e) => {
+              setForm((prev) => ({ ...prev, first_name: e.target.value }));
+            }} ></input>
         </div>
         <div>
           <label></label>
-          <input type="string" placeholder="last name" 
-          onChange={(e) => {
-            setForm((prev) => ({ ...prev, last_name: e.target.value }));
-          }}></input>
+          <input type="string" placeholder="last name"
+            onChange={(e) => {
+              setForm((prev) => ({ ...prev, last_name: e.target.value }));
+            }}></input>
         </div>
         <label></label>
         <input type="string" placeholder="email"
-        onChange={(e) => {
+          onChange={(e) => {
             setForm((prev) => ({ ...prev, email: e.target.value }));
           }}></input>
-          <div>
-           <input type="string" placeholder="comment"
-        onChange={(e) => {
-            setForm((prev) => ({ ...prev, comment: e.target.value }));
-          }}></input>
-          </div>
+        <div>
+          <input type="string" placeholder="comment"
+            onChange={(e) => {
+              setForm((prev) => ({ ...prev, comment: e.target.value }));
+            }}></input>
+        </div>
         <div>
           <label>date</label>
           <input type="date" placeholder="date"
-          onChange={(e) => {
-            setForm((prev) => ({ ...prev, schedule_date: e.target.value }));
-          }}></input>
+            onChange={(e) => {
+              setForm((prev) => ({ ...prev, schedule_date: e.target.value }));
+            }}></input>
         </div>
         <button> submit</button>
       </form>
       <button
-       onClick={()=>selectPerson(comment.id)}
+        onClick={() => selectPerson(comment.id)}
       >
         update
       </button>
 
     </div>
-    )
-      
+  )
+
 
 };
 
