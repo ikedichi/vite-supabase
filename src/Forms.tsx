6be8789c72d1af4cd98d64abb5ec464bbba3333c
Victor <c.ikedichi@gmail.com>
2023-01-11 13:47:52 -0500

    Getting closer

--- src/Forms.tsx
+++ src/Forms.tsx
@@ -17,104 +17,104 @@ type Comment = {
 
 function Forms() {
   // const [is_deleted, setIs_Deleted] = useState(false)
-   const  [isEmpty, setIsEmpty] = useState(false);
-    const [comments, setComments] = useState<Comment[]>();
-    const [form, setForm] = useState<any>({});
-    const [firstName, SetFirstName] = useState('');
-    const [lastName, SetLastName] = useState('');
-    const [email, SetEmail] = useState('');
-    const [comment, setComment] = useState('')
-    const [date, setEnteredDate]=useState('');
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [comments, setComments] = useState<Comment[]>();
+  const [form, setForm] = useState<any>({});
+  const [firstName, SetFirstName] = useState('');
+  const [lastName, SetLastName] = useState('');
+  const [email, SetEmail] = useState('');
+  const [comment, setComment] = useState('')
+  const [date, setEnteredDate] = useState('');
 
 
-    useEffect(() => {
-      getData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-async function getData(){
+  async function getData() {
     try {
-      const {data, error} = await supabase
-      .from('comments')
-      .select()
-      if(error) throw error;
-      if(data !=null){
+      const { data, error } = await supabase
+        .from('comments')
+        .select()
+      if (error) throw error;
+      if (data != null) {
         setComments(data)
       }
     }
-    catch(error){
+    catch (error) {
       alert(error.message)
     }
-}
+  }
 
 
-    
-    // const getData = async (commentId: number) => {
-    //   const result = await fetch(
-    //     'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments' ,
-    //     {
-    //       headers: {
-    //         apikey:
-    //           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-    //       },
-    //     },
-    //     // SetFirstName(result.first_name)
-    //   );
-    //   const data = await result.json() as Comment[] ;
-    //   const filteredData = data.filter( comment=>comment);
-    //   setComments(filteredData);
-    //   // console.log(data)
-    // };
-    
 
-    async function onFormSubmit(){
-      try {
-        const {data, error} = await supabase
+  // const getData = async (commentId: number) => {
+  //   const result = await fetch(
+  //     'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments' ,
+  //     {
+  //       headers: {
+  //         apikey:
+  //           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
+  //       },
+  //     },
+  //     // SetFirstName(result.first_name)
+  //   );
+  //   const data = await result.json() as Comment[] ;
+  //   const filteredData = data.filter( comment=>comment);
+  //   setComments(filteredData);
+  //   // console.log(data)
+  // };
+
+
+  async function onFormSubmit() {
+    try {
+      const { data, error } = await supabase
         .from('comments')
         .insert({
           first_name: firstName,
           last_name: lastName,
           email: email,
-          comment:comment,
+          comment: comment,
           date: date
-          
+
 
         })
         .single()
-        if(error) throw error;
-        window;location.reload();
-      }
-      catch(error){
-        alert(error.message)
-      }
+      if (error) throw error;
+      window; location.reload();
+    }
+    catch (error) {
+      alert(error.message)
+    }
   }
 
 
-  
-    // async function onFormSubmit(event: React.FormEvent) {
-    //   // event.preventDefault();
-    //   if(!form.first_name || !form.last_name || !form.email || !form.comment ){
-    //     console.log('please fill in all fields')
-    //     return;
-        
-    //   } else{console.log('w');}
-  
-    //   const result = await fetch(
-    //     'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments',
-    //     {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //         apikey:
-    //           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-    //       },
-    //       body: JSON.stringify(form),
-    //     }
-    //   );
-    //   console.log(result);
-    //   getData();
-      
 
-    // }
+  // async function onFormSubmit(event: React.FormEvent) {
+  //   // event.preventDefault();
+  //   if(!form.first_name || !form.last_name || !form.email || !form.comment ){
+  //     console.log('please fill in all fields')
+  //     return;
+
+  //   } else{console.log('w');}
+
+  //   const result = await fetch(
+  //     'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments',
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         apikey:
+  //           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
+  //       },
+  //       body: JSON.stringify(form),
+  //     }
+  //   );
+  //   console.log(result);
+  //   getData();
+
+
+  // }
 
 
 
@@ -133,118 +133,97 @@ async function getData(){
   //     }
   // }
 
-  }
 
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+// useEffect(() => {
+//   getData();
+// }, []);
 
-  // const getData = async (commentId: number) => {
-  //   const result = await fetch(
-  //     'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments',
-  //     {
-  //       headers: {
-  //         apikey:
-  //           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-  //       },
-  //     },
-  //     // SetFirstName(result.first_name)
-  //   );
-  //   const data = await result.json() as Comment[];
-  //   const filteredData = data.filter(comment => comment);
-  //   setComments(filteredData);
-  //   // console.log(data)
-  // };
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-
-
-  async function onFormSubmit() {
-    try {
-      const { error } = await supabase
-        .from('comments')
-        .insert({ name:firstName,
-                  last: lastName,
-            // comment: comments,
-                  schedule_date: date,
-            email: email,})
-      if (error) throw error;
-      if (data != null) {
-      }
-    } catch (error) {
-      console.log(error.message)
-    }
-    getData();
-  }
+// const getData = async (commentId: number) => {
+//   const result = await fetch(
+//     'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments',
+//     {
+//       headers: {
+//         apikey:
+//           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
+//       },
+//     },
+//     // SetFirstName(result.first_name)
+//   );
+//   const data = await result.json() as Comment[];
+//   const filteredData = data.filter(comment => comment);
+//   setComments(filteredData);
+//   // console.log(data)
+// };
+useEffect(() => {
+  getData();
+}, []);
 
 
+// async function onFormSubmit() {
+//   try {
+//     const { error } = await supabase
+//       .from('comments')
+//       .insert({
+//         name: firstName,
+//         last: lastName,
+//         // comment: comments,
+//         schedule_date: date,
+//         email: email,
+//       })
+//     if (error) throw error;
+//     if (data != null) {
+//     }
+//   } catch (error) {
+//     console.log(error.message)
+//   }
+//   getData();
+// }
 
 
-    async function isDeleted(commentId: number) {
-        // TODO: Add code to update comment
-        // let is_deleted = true
-        const result = await fetch(
-            'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments?id=eq.' + commentId,
-            {
-              method: 'PATCH',
-              headers: {
-                'Content-Type': 'application/json',
-                apikey:
-                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-                  },
-                  body: JSON.stringify({ is_deleted: true })
-              // comments.is_deleted
-            } 
-        ) 
-        getData();
-        // { comments?.map(comment=>{comment.is_deleted = true})
-      }
-    );
 
-    getData();
-  }
 
-  async function isDeleted(commentId: number) {
-    // TODO: Add code to update comment
-    let is_deleted = true
-    const result = await fetch(
-      'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments?id=eq.' + commentId,
-      {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          apikey:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
-        },
-        body: JSON.stringify({ is_deleted: true })
+async function isDeleted(commentId: number) {
+  // TODO: Add code to update comment
+  // let is_deleted = true
+  const result = await fetch(
+    'https://avvidhxhjmaskwwewvey.supabase.co/rest/v1/comments?id=eq.' + commentId,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        apikey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2dmlkaHhoam1hc2t3d2V3dmV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzc3MDgsImV4cCI6MTk4NjYxMzcwOH0.urgAvh6ctpOFwvZyBtl0JlIT-3Axvw_9eKBQhtFrmDY',
       },
-
-    )
-    // getData();
-    // { comments?.map(comment=>{comment.is_deleted = true})
-  }
-
-
-
-
-       
-        
+      body: JSON.stringify({ is_deleted: true })
+      // comments.is_deleted
+    }
+  )
+  getData();
+  // { comments?.map(comment=>{comment.is_deleted = true})
+}
 
 
-  //   function isDeleted(commentId: number){ comments?.map(comment=>{comment.is_deleted = true})
-  // }
-
-  function selectPerson() {
-
-  }
 
 
-  return (
-    <div>
 
-{/* <table border={1}>
+
+
+
+
+
+//   function isDeleted(commentId: number){ comments?.map(comment=>{comment.is_deleted = true})
+// }
+
+function selectPerson() {
+
+}
+
+
+return (
+  <div>
+
+    {/* <table border={1}>
       
         <tr>
           <th>ID</th>
@@ -279,50 +258,50 @@ async function getData(){
 
 
 
-      <form onSubmit={onFormSubmit} id="form">
-        <div>
-          <input id="first_name" placeholder="first name"
+    <form onSubmit={onFormSubmit} id="form">
+      <div>
+        <input id="first_name" placeholder="first name"
 
-            onChange={(e) => {
-              setForm((prev) => ({ ...prev, first_name: e.target.value }));
-            }} ></input>
-        </div>
-        <div>
-          <label></label>
-          <input type="string" placeholder="last name"
-            onChange={(e) => {
-              setForm((prev) => ({ ...prev, last_name: e.target.value }));
-            }}></input>
-        </div>
-        <label></label>
-        <input type="string" placeholder="email"
           onChange={(e) => {
-            setForm((prev) => ({ ...prev, email: e.target.value }));
+            setForm((prev) => ({ ...prev, first_name: e.target.value }));
+          }} ></input>
+      </div>
+      <div>
+        <label></label>
+        <input type="string" placeholder="last name"
+          onChange={(e) => {
+            setForm((prev) => ({ ...prev, last_name: e.target.value }));
           }}></input>
-        <div>
-          <input type="string" placeholder="comment"
-            onChange={(e) => {
-              setForm((prev) => ({ ...prev, comment: e.target.value }));
-            }}></input>
-        </div>
-        <div>
-          <label>date</label>
-          <input type="date" placeholder="date"
-            onChange={(e) => {
-              setForm((prev) => ({ ...prev, schedule_date: e.target.value }));
-            }}></input>
-        </div>
-        <button onClick={()=>onFormSubmit()}> submit</button>
-      </form>
-      <button
-        onClick={() => selectPerson(comment.id)}
-      >
-        update
-      </button>
+      </div>
+      <label></label>
+      <input type="string" placeholder="email"
+        onChange={(e) => {
+          setForm((prev) => ({ ...prev, email: e.target.value }));
+        }}></input>
+      <div>
+        <input type="string" placeholder="comment"
+          onChange={(e) => {
+            setForm((prev) => ({ ...prev, comment: e.target.value }));
+          }}></input>
+      </div>
+      <div>
+        <label>date</label>
+        <input type="date" placeholder="date"
+          onChange={(e) => {
+            setForm((prev) => ({ ...prev, schedule_date: e.target.value }));
+          }}></input>
+      </div>
+      <button onClick={() => onFormSubmit()}> submit</button>
+    </form>
+    <button
+      onClick={() => selectPerson(comment.id)}
+    >
+      update
+    </button>
 
-    </div>
-  )
-
+  </div>
+)
+      
 
 };
 
